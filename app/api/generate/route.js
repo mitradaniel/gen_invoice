01570da3da9a116221192c5049f3d296d05785fe
mitradaniel <73757072+mitradaniel@users.noreply.github.com>
2026-04-15 14:46:02 +0530

    Update route.js

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -66,7 +66,7 @@ export async function POST(req) {
     /* ================= HEADER RIGHT ================= */
     page.drawText(`Date: ${date}`, {
       x: width - 180,
-      y: 500,
+      y: 600,
       size: 10,
       font
     });
