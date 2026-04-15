d501b8dcb979e17a407f5e267a2725547041b4c7
mitradaniel <73757072+mitradaniel@users.noreply.github.com>
2026-04-15 14:55:59 +0530

    Update route.js

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -81,7 +81,7 @@ export async function POST(req) {
     /* ================= SUBJECT (ALIGN WITH HEADER) ================= */
     page.drawText(`Subject: ${subject}`, {
       x: 50,
-      y: 730,
+      y: 670,
       size: 12,
       font: bold
     });
